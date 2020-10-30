e7e7dd952f7127d7b2ed7689dcb5f5b7606182dc
davidtrigo <d.trigo83@gmail.com>
2020-10-31 00:03:04 +0100

    seeder factory

---
+++ database/seeds/TransactionTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class TransactionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Category::truncate();
+        Transaction::truncate();
+        Product::truncate();
+        DB::table('category_product')->truncate();
+
+        $cantUsers = 1000;
+        $cantCategories = 30;
+        $cantProducts = 1000;
+        $cantTransactions = 1000;
+
+        factory(User::class, $cantUsers)->create();
+
+        $categories = factory(Category::class, $cantCategories)->create();
+
+        factory(Product::class, $cantProducts)->create()->each(
+            function ($product) use ($categories) {
+                $randomCategories = $categories->random(mt_rand(1, 5))->pluck('id');
+
+                $product->categories()->attach($randomCategories);
+            }
+        );
+
+        factory(Transaction::class, $cantTransactions)->create();
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
