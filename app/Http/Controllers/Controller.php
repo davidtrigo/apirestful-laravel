f75c8b472833afd859115f4a411a41adc3c72ab2
davidtrigo <d.trigo83@gmail.com>
2020-10-31 01:02:49 +0100

    register apiResponser

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-//use App\Traits\ApiResponser;
+use App\Traits\ApiResponser;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
